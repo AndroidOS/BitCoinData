0d8278cff839b9cb17dd4f460b2fe62a7eacfbca
AndroidOS <burrowsgit@gmail.com>
2020-01-18 08:33:45 +1100

    Initial Commit

---
+++ BitCoinData/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BitCoinData
+//
+//  Created by Manuel Carvalho on 18/1/20.
+//  Copyright © 2020 Manuel Carvalho. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
